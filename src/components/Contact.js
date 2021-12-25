a3d804b1ca4e2903de8fe5de9188988f1de24e70
Natalie Pitts <nataliepittss@gmail.com>
2021-12-25 19:17:36 +0200

    remove form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,8 +64,9 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <form
+        {/* <form
           netlify
+          data-netlify="true"
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -117,7 +118,7 @@ export default function Contact() {
             className="text-purple-lightest bg-purple-dark border-0 py-2 px-6 focus:outline-none hover:bg-purple-darkest rounded text-lg">
             Submit
           </button>
-        </form>
+        </form> */}
       </div>
     </section>
   );
