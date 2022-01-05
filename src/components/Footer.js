1a3cfb92eff551931b7d96c50ae613b5df98cd0b
Natalie Pitts <nataliepittss@gmail.com>
2022-01-05 16:16:14 +0200

    change to 2022

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="container px-2 py-1 mx-auto text-center lg:px-2">
         <div className="flex flex-col mb-50">
           <h1 className="sm:text-lg text-lg font-medium title-font mb-4 text-purple-darkest">
-          © 2021 Natalie Pitts
+          © 2022 Natalie Pitts
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
          Made with React.js
