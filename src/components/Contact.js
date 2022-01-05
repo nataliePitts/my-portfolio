8cb2cdb2d576497912d16513a54277214ff2bb65
Natalie Pitts <nataliepittss@gmail.com>
2022-01-05 16:39:56 +0200

    form debbuging

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,9 +8,7 @@ export default function Contact() {
 
   function encode(data) {
     return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
+      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
       .join("&");
   }
 
@@ -19,7 +17,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
+      body: encode({ "form-name": "test", name, email, message }),
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
@@ -64,10 +62,9 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        {/* <form
+        <form
           netlify
-          data-netlify="true"
-          name="contact"
+          name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-gray-500 sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -118,7 +115,7 @@ export default function Contact() {
             className="text-purple-lightest bg-purple-dark border-0 py-2 px-6 focus:outline-none hover:bg-purple-darkest rounded text-lg">
             Submit
           </button>
-        </form> */}
+        </form>
       </div>
     </section>
   );
