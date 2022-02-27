4f3996206af0357a1d03cef913cb961c1d261bd9
Natalie Pitts <nataliepittss@gmail.com>
2022-02-28 00:15:48 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ export default function App() {
         <Route path="/Posts" element={<Posts />}/>
         <Route path="/Contact" element={<Contact />} />
       </Routes>
+      <a
+        href="https://wa.me/9720547769369"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
       <Footer/>
       </BrowserRouter>
   );
