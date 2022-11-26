ed74e32a440fb9081e1027b800868b506efb02af
Natalie Pitts <nataliepittss@gmail.com>
2022-11-26 21:06:04 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,16 @@ export default function Footer() {
   <footer className="bg-purple-light">
       <div className="container px-2 py-1 mx-auto text-center lg:px-2">
         <div className="flex flex-col mb-50">
+<<<<<<< HEAD
           <h1 className="sm:text-sm text-sm font-medium title-font mb-4 text-purple-darkest">
            Natalie Pitts 2022 © Created by React.js
+=======
+          <h1 className="sm:text-lg text-lg font-medium title-font mb-4 text-purple-darkest">
+          © 2022 Natalie Pitts
+>>>>>>> parent of f233ffa4 (footer udt)
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-        
+         Made with React.js
           </p>
         </div>
       </div>
