221248bdbf0398a0d7271dc5e1e69fceaa115933
Natalie Pitts <nataliepittss@gmail.com>
2023-02-24 18:45:02 +0200

    style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Footer() {
     return (
 
-  <footer className="bg-newwinter-light">
+  <footer className="bg-newwinter-darkest">
       <div className="container px-2 py-1 mx-auto text-center lg:px-2">
         <div className="flex flex-col mb-50">
-          <h1 className="sm:text-sm text-sm font-medium title-font mb-4 text-newwinter-darkest">
+          <h1 className="sm:text-sm text-sm font-medium title-font mb-4 text-newwinter-light">
           <b>Natalie Pitts </b> <br/>2022 © Created with React.js 
           </h1>
         </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -19,9 +19,9 @@ export default function Skills() {
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-newwinter-light rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-newwinter-lightest w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium text-white">
+              <div className="bg-newwinter-dark rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-newwinter-light w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-newwinter-lightest">
                   {skill}
                 </span>
               </div>
