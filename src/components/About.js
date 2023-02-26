56a8aacd53c141d1b3fc0e796487f21b617d29ff
Natalie Pitts <nataliepittss@gmail.com>
2023-02-26 21:23:05 +0200

    style

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,17 @@ import CTA from './CTA'
 
 export default function About() {
   return (
-    <section id="about bg-newwinter-light">
+    <section id="about" className="bg-newwinter-lightest">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-newwinter-darkest">
              Hello!
-            <br className="hidden lg:inline-block text-newwinter-dark" /> <br/> 
+            <br className="hidden lg:inline-block text-newwinter-darkest" /> 
           </h1>
-          <p className="mb-8 leading-relaxed text-newwinter-dark">
+          <p className="mb-8 leading-relaxed text-newwinter-darkest">
           <br/>It's a pleasure to meet you.
-          <br/>
-          <br/>My name is Natalie, and I'm a<b className="text-newwinter-darkest"> Data Science and Internet Technology Specialist </b> <br/><br/> Who enjoys creating and developing things from the scratch, as well as bringing ideas to life in the digital world using data, frontend development, and UX/UI design.
+          <br/><br/>
+          <b className="text-newwinter-darkest">Front End Developer </b> <br/><br/> Who enjoys creating and developing things from the scratch, as well as bringing ideas to life in the digital world using web development and UX/UI design.
           <br/>
           <br/>
           <p class="italic">"The only way to do great work is to love what you do."  <br/>- Steve Jobs </p>
