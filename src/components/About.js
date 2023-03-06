628813db356844187ca9b0e05f26b2ad1b87070f
Natalie Pitts <nataliepittss@gmail.com>
2023-03-06 19:40:07 +0200

    dev gif

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ export default function About() {
           <p className="mb-8 leading-relaxed text-newwinter-darkest">
           <br/>It's a pleasure to meet you.
           <br/><br/>
-          <b className="text-newwinter-darkest">Front End Developer </b> <br/><br/> Who enjoys creating and developing things from scratch, as well as bringing ideas to life in the digital world using Web development and UI/UX design.
+          <b className="text-newwinter-darkest">I'm a Front End Developer </b> <br/><br/> Who enjoys creating and developing things from scratch, as well as bringing ideas to life in the digital world using Web development and UI/UX design.
           <br/>
           <br/>
           <p class="italic">“We’re here to put a dent in the universe. Otherwise why else even be here?”  <br/>- Steve Jobs </p>
@@ -33,6 +33,7 @@ export default function About() {
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               Projects
             </a> */}
+            
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
